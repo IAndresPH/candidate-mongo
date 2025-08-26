74d0a3617aef073c094af7c40ad86d798ba7950d
IAndresPH <amolinaph@gmail.com>
2025-08-26 11:35:35 -0500

    initial(project): add project base

---
+++ src/main/java/com/interview/backend/mongo/CandidateBackendMongoApplication.java
@@ -0,0 +1,12 @@
+package com.interview.backend.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateBackendMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateBackendMongoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/interview/backend/mongo/CandidateBackendMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.backend.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateBackendMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
